e2c9a78fef8fcb27458a2324ca0237952da98be2
侯恩星 <houenxing@houenxingdeMacBook-Pro.local>
2018-08-14 18:51:01 +0800

    update

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+var config = require("../config");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log("Backend called!");
+
+  console.log(config.sql.host);
+
+  let connection = mysql.createConnection({
+      host: config.host,
+      user: config.user,
+      password: config.password,
+      port: config.port
+  });
+
+  connection.connect( err => {
+    console.log("Error when connecting to db:", err);
+  })
+
+});
+
+module.exports = router;
